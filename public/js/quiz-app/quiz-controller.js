789f934b701daa73bbcf8b565d563ecf8cb5745b
jmhmd <jhostetter@gmail.com>
2014-04-06 18:27:39 -0400

    added images

--- public/js/quiz-app/quiz-controller.js
+++ public/js/quiz-app/quiz-controller.js
@@ -3,36 +3,70 @@
 /* Controllers */
 
 var quizApp = angular.module('quizApp.controllers', [])	
-quizApp.controller('questionCtrl', ['$scope',
-	function($scope) {
+quizApp.controller('questionCtrl', ['$scope', '$http',
+	function($scope, $http) {
 
 		// make linter happy
 		//var quiz = quiz
 		
 		$scope.quiz = quiz
 
-		$scope.currentIndex = 0
+
+		var _init = function(){
+			
+			$scope.currentIndex = 0
+
+			// may want to preload all images in background at some point
+			_loadImage($scope.currentIndex)
+		}
 
 		$scope.currentQuestion = function(){
 
-			console.log('current question is: ', $scope.currentIndex, $scope.quiz.questions[$scope.currentIndex])
+			//console.log('current question is: ', $scope.currentIndex, $scope.quiz.questions[$scope.currentIndex])
 
 			return $scope.quiz.questions[$scope.currentIndex]
 		}
 
 		$scope.gotoQuestion = function(index){
 
-			console.log('goto question: ', index)
-
-			if (!$scope.isCurrentQuestion(index)){
-
-				$scope.currentIndex = index
+			if ($scope.isCurrentQuestion(index)){
+				return false
 			}
+			
+			console.log('goto question: ', index)
+			
+			$scope.currentIndex = index
+
+			if (!$scope.quiz.questions[index].imageSeries){
+				_loadImage(index)
+			}
+
 		}
 
 		$scope.isCurrentQuestion = function(index){
 
 			return index === $scope.currentIndex
 		}
+
+		var _loadImage = function(index){
+
+			var studyId = $scope.quiz.questions[index].studyId
+
+			console.log('loading image with study id: ', studyId)
+
+			$http.get('/api/getImageObject/' + studyId)
+				.success(function(res) {
+
+					console.log('loaded image: ', res)
+
+					$scope.quiz.questions[index].imageSeries = res.imageStacks
+				})
+				.error(function(err) {
+
+					console.error(err)
+				})
+		}
+
+		_init()
 	}
 ])
